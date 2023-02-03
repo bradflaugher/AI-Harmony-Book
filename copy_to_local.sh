19ca510ecc28062f2de4768b537efc98892f44ef
Brad Flaugher <brad@bradflaugher.com>
2023-02-03 15:56:25 +0000

    use github to copy

--- copy_to_local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-scp brad@192.168.0.170:/home/brad/src/dfp-book/main.pdf ~/Downloads/FSD_Book.pdf
-#scp brad@stack.bradflaugher.com:/home/brad/src/dfp-book/main.pdf ~/Downloads/FSD_Book.pdf
-     
-
--- copy_to_local_external.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp brad@stack.bradflaugher.com:/home/brad/src/dfp-book/main.pdf ~/Downloads/FSD_Book.pdf
-     
-
