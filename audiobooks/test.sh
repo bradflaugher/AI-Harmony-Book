f7cf9ff8c32a9e0547e954396de668bad5ddeadd
Brad Flaugher <brad@bradflaugher.com>
2023-04-17 19:24:36 +0000

    audiobooks

---
+++ audiobooks/test.sh
@@ -0,0 +1,22 @@
+tts --text "There is little doubt that artificial intelligence will contribute to profound transformations over the next decades. At its best, the technology has the potential to release us from mundane work and create a utopia in which all time is leisure time. At its worst, World War 3 might be fought by armies of superintelligent robots. But they won't be led by Hal, Skynet or their newer artificially intelligent relatives. Even in the worst case, the robots will remain under our command, and we will have only ourselves to blame." \
+    --model_name "tts_models/en/ljspeech/vits" \
+    --out_path /tmp/output.wav
+
+#  7: tts_models/en/ek1/tacotron2
+# 8: tts_models/en/ljspeech/tacotron2-DDC
+# 9: tts_models/en/ljspeech/tacotron2-DDC_ph
+# 10: tts_models/en/ljspeech/glow-tts
+# 11: tts_models/en/ljspeech/speedy-speech
+# 12: tts_models/en/ljspeech/tacotron2-DCA [already downloaded]
+# 13: tts_models/en/ljspeech/vits
+# 14: tts_models/en/ljspeech/vits--neon
+# 15: tts_models/en/ljspeech/fast_pitch
+# 16: tts_models/en/ljspeech/overflow
+# 17: tts_models/en/ljspeech/neural_hmm
+# 18: tts_models/en/vctk/vits
+# 19: tts_models/en/vctk/fast_pitch
+# 20: tts_models/en/sam/tacotron-DDC
+# 21: tts_models/en/blizzard2013/capacitron-t2-c50
+# 22: tts_models/en/blizzard2013/capacitron-t2-c150_v2
+
+
---
+++ audiobooks/test2.sh
@@ -0,0 +1,23 @@
+tts --text "Section 1, The AI Renaissance. Technology often finds its first adopters among those who can afford it and foresee its potential. Artificial Intelligence is no exception, with its early applications dating back several decades, notably in the financial industry. Hedge funds and investment banks identified the value of AI and started using it for trading and portfolio management.
+" \
+    --model_name "tts_models/en/ljspeech/vits" \
+    --out_path /tmp/output2.wav
+
+#  7: tts_models/en/ek1/tacotron2
+# 8: tts_models/en/ljspeech/tacotron2-DDC
+# 9: tts_models/en/ljspeech/tacotron2-DDC_ph
+# 10: tts_models/en/ljspeech/glow-tts
+# 11: tts_models/en/ljspeech/speedy-speech
+# 12: tts_models/en/ljspeech/tacotron2-DCA [already downloaded]
+# 13: tts_models/en/ljspeech/vits
+# 14: tts_models/en/ljspeech/vits--neon
+# 15: tts_models/en/ljspeech/fast_pitch
+# 16: tts_models/en/ljspeech/overflow
+# 17: tts_models/en/ljspeech/neural_hmm
+# 18: tts_models/en/vctk/vits
+# 19: tts_models/en/vctk/fast_pitch
+# 20: tts_models/en/sam/tacotron-DDC
+# 21: tts_models/en/blizzard2013/capacitron-t2-c50
+# 22: tts_models/en/blizzard2013/capacitron-t2-c150_v2
+
+
