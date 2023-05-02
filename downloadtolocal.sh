52b6e7be6e1317e5e61548d7075275a0ec7daa1b
Brad Flaugher <brad@bradflaugher.com>
2023-05-02 12:49:50 +0000

    local

--- downloadtolocal.sh
+++ downloadtolocal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp brad@192.168.50.200:/home/brad/src/ai-book/main.pdf /home/brad/Downloads/main.pdf
+scp brad@192.168.50.71:/home/brad/src/ai-book/main.pdf /home/brad/Downloads/main.pdf
